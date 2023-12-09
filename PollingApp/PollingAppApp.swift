2c07cd43eb8b276374d6548c3072406202168eff
Sergey Teryokhin <sergey_teryokhin@epam.com>
2023-12-09 16:22:46 +0100

    Initial Commit

---
+++ PollingApp/PollingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PollingAppApp.swift
+//  PollingApp
+//
+//  Created by Sergey Teryokhin on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PollingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
